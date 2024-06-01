dd31b5c1e66b8dd906edf8e8959c4062080e915e
Adam Patterson <adam@adamrt.com>
2024-06-01 16:58:17 -0700

    Add and use Renderable and Scene classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include "glm/ext/matrix_transform.hpp"
+#include <iostream>
+#include <random>
+#include <vector>
+
 #define SOKOL_IMPL
 #define SOKOL_GLCORE
 #include "sokol_app.h"
@@ -13,6 +18,139 @@
 #include "glm/gtc/matrix_transform.hpp"
 
 #include "shader.glsl.h"
+
+float random_float(float min, float max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(min, max);
+    return dis(gen);
+}
+
+class Renderable {
+public:
+    ~Renderable() = default;
+
+    Renderable(const float* vertices, size_t vertex_size, const uint16_t* indices, size_t index_size, sg_shader shader)
+        : model_matrix(1.0f)
+    {
+        sg_buffer_desc vbuf_desc = {};
+        vbuf_desc.data = sg_range { vertices, vertex_size };
+        vbuf_desc.label = "vertex-buffer";
+        vertex_buffer = sg_make_buffer(&vbuf_desc);
+
+        sg_buffer_desc ibuf_desc = {};
+        ibuf_desc.type = SG_BUFFERTYPE_INDEXBUFFER;
+        ibuf_desc.data = sg_range { indices, index_size };
+        ibuf_desc.label = "index-buffer";
+        index_buffer = sg_make_buffer(&ibuf_desc);
+
+        sg_pipeline_desc pip_desc = {};
+        pip_desc.shader = shader;
+        pip_desc.index_type = SG_INDEXTYPE_UINT16;
+        pip_desc.cull_mode = SG_CULLMODE_BACK;
+        pip_desc.label = "cube-pipeline";
+
+        pip_desc.layout = {};
+        pip_desc.layout.buffers[0].stride = 28;
+        pip_desc.layout.attrs[ATTR_vs_position].format = SG_VERTEXFORMAT_FLOAT3;
+        pip_desc.layout.attrs[ATTR_vs_color0].format = SG_VERTEXFORMAT_FLOAT4;
+
+        pip_desc.depth = {};
+        pip_desc.depth.write_enabled = true;
+        pip_desc.depth.compare = SG_COMPAREFUNC_LESS_EQUAL;
+
+        pipeline = sg_make_pipeline(&pip_desc);
+
+        bindings.vertex_buffers[0] = vertex_buffer;
+        bindings.index_buffer = index_buffer;
+        num_indices = index_size / sizeof(uint16_t);
+
+        std::uniform_real_distribution<> dis(1.0f, 10.0f);
+        rspeed = random_float(1.0f, 3.0f);
+    }
+
+    auto render(const glm::mat4& view_proj) -> void
+    {
+        glm::mat4 mvp = view_proj * model_matrix;
+        vs_params_t vs_params;
+        vs_params.mvp = mvp;
+
+        sg_apply_pipeline(pipeline);
+        sg_apply_bindings(&bindings);
+
+        sg_range vs_range = SG_RANGE(vs_params);
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, &vs_range);
+        sg_draw(0, num_indices, 1);
+    }
+
+    auto update(float delta_time) -> void
+    {
+        auto x = 1.0f * delta_time * rspeed;
+        auto y = 2.0f * delta_time * rspeed;
+
+        rotate(x, glm::vec3(1.0f, 0.0f, 0.0f));
+        rotate(y, glm::vec3(0.0f, 1.0f, 0.0f));
+    }
+
+    auto set_model_matrix(const glm::mat4& matrix) -> void
+    {
+        model_matrix = matrix;
+    }
+
+    auto translate(const glm::vec3& translation) -> void
+    {
+        model_matrix = glm::translate(model_matrix, translation);
+    }
+
+    auto rotate(float angle, const glm::vec3& axis) -> void
+    {
+        model_matrix = glm::rotate(model_matrix, angle, axis);
+    }
+
+    auto scale(const glm::vec3& scaling_factors) -> void
+    {
+        model_matrix = glm::scale(model_matrix, scaling_factors);
+    }
+
+private:
+    sg_pipeline pipeline = {};
+    sg_bindings bindings = {};
+    sg_buffer vertex_buffer = {};
+    sg_buffer index_buffer = {};
+    int num_indices = 0;
+    glm::mat4 model_matrix;
+
+    float rspeed;
+};
+
+class Scene {
+public:
+    Scene() = default;
+    ~Scene() = default;
+
+    auto add_renderable(const Renderable& renderable) -> void
+    {
+        renderables.push_back(renderable);
+    }
+
+    auto update(float delta_time) -> void
+    {
+        for (auto& renderable : renderables) {
+            renderable.update(delta_time);
+        }
+    }
+
+    auto render(const glm::mat4& view_proj) -> void
+    {
+        for (auto& renderable : renderables) {
+            renderable.render(view_proj);
+        }
+    }
+
+private:
+    std::vector<Renderable> renderables;
+};
 
 enum class Projection : int {
     Perspective = 0,
@@ -123,6 +261,7 @@ static struct {
     float rx, ry;
 
     Camera camera;
+    Scene scene;
 
     struct {
         sg_color clear_color = { 0.0f, 0.5f, 0.7f, 1.0f };
@@ -132,13 +271,133 @@ static struct {
     } gfx;
 } state;
 
-void gfx_init()
+auto gfx_init() -> void
 {
     sg_desc desc = {};
     desc.environment = sglue_environment();
     desc.logger.func = slog_func;
     sg_setup(&desc);
 
+    state.gfx.pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
+}
+
+auto gui_init() -> void
+{
+    simgui_desc_t simgui_desc = {};
+    simgui_desc.logger.func = slog_func;
+    simgui_setup(&simgui_desc);
+}
+
+Renderable create_cube(sg_shader shader);
+
+auto init() -> void
+{
+    gfx_init();
+    gui_init();
+
+    sg_shader cube_shader = sg_make_shader(cube_shader_desc(sg_query_backend()));
+
+    Renderable cube1 = create_cube(cube_shader);
+    cube1.translate(glm::vec3(-2.0f, 0.0f, 0.0f));
+    state.scene.add_renderable(cube1);
+
+    Renderable cube2 = create_cube(cube_shader);
+    cube2.translate(glm::vec3(2.0f, 0.0f, 0.0f));
+    state.scene.add_renderable(cube2);
+}
+
+auto gui_draw() -> void
+{
+    ImGui::SetNextWindowSize(ImVec2(0, 0));
+
+    ImGui::Begin("Hello, world!");
+    if (ImGui::RadioButton("Perspective", state.camera.projection == Projection::Perspective)) {
+        state.camera.projection = Projection::Perspective;
+    }
+    ImGui::SameLine();
+    if (ImGui::RadioButton("Orthographic", state.camera.projection == Projection::Orthographic)) {
+        state.camera.projection = Projection::Orthographic;
+    }
+    ImGui::SliderFloat("Rotation", &state.rotation_speed, 0.0f, 2.0f);
+    ImGui::ColorEdit3("Background", &state.gfx.clear_color.r);
+    if (ImGui::Button(sapp_is_fullscreen() ? "Switch to windowed" : "Switch to fullscreen")) {
+        sapp_toggle_fullscreen();
+    }
+    ImGui::End();
+}
+
+auto frame() -> void
+{
+    const float t = (float)sapp_frame_duration();
+
+    state.camera.update();
+    state.scene.update(t);
+
+    simgui_new_frame({
+        sapp_width(),
+        sapp_height(),
+        sapp_frame_duration(),
+        sapp_dpi_scale(),
+    });
+
+    state.gfx.pass_action.colors[0].clear_value = state.gfx.clear_color;
+
+    sg_pass pass = {};
+    pass.action = state.gfx.pass_action;
+    pass.swapchain = sglue_swapchain();
+
+    sg_begin_pass(&pass);
+    {
+        glm::mat4 view_proj = state.camera.proj_matrix() * state.camera.view_matrix();
+        state.scene.render(view_proj);
+        gui_draw();
+        simgui_render();
+    }
+    sg_end_pass();
+
+    sg_commit();
+}
+
+auto cleanup() -> void
+{
+    simgui_shutdown();
+    sg_shutdown();
+}
+
+auto input(sapp_event const* event) -> void
+{
+    if (event->type == SAPP_EVENTTYPE_KEY_DOWN) {
+        if (event->key_code == SAPP_KEYCODE_ESCAPE) {
+            sapp_quit();
+        }
+    }
+
+    if (simgui_handle_event(event)) {
+        return;
+    }
+
+    state.camera.handle_event(event);
+}
+
+sapp_desc sokol_main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+    sapp_desc desc = {};
+    desc.init_cb = init;
+    desc.frame_cb = frame;
+    desc.cleanup_cb = cleanup;
+    desc.event_cb = input;
+    desc.window_title = "Dear ImGui (sokol-app)";
+    desc.ios_keyboard_resizes_canvas = false;
+    desc.icon.sokol_default = true;
+    desc.enable_clipboard = true;
+    desc.logger.func = slog_func;
+    return desc;
+}
+
+Renderable create_cube(sg_shader shader)
+{
     // clang-format off
     float vertices[] = {
         -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
@@ -182,153 +441,6 @@ void gfx_init()
     };
     // clang-format on
 
-    sg_shader shd = sg_make_shader(cube_shader_desc(sg_query_backend()));
-
-    sg_buffer_desc vbuf_desc = {};
-    vbuf_desc.data = SG_RANGE(vertices);
-    vbuf_desc.label = "cube-vertices";
-    sg_buffer vbuf = sg_make_buffer(&vbuf_desc);
-
-    sg_buffer_desc ibuf_desc = {};
-    ibuf_desc.type = SG_BUFFERTYPE_INDEXBUFFER;
-    ibuf_desc.data = SG_RANGE(indices);
-    ibuf_desc.label = "cube-indices";
-    sg_buffer ibuf = sg_make_buffer(&ibuf_desc);
-
-    sg_pipeline_desc pip_desc = {};
-    pip_desc.shader = shd;
-    pip_desc.index_type = SG_INDEXTYPE_UINT16;
-    pip_desc.cull_mode = SG_CULLMODE_BACK;
-    pip_desc.label = "cupe-pipeline";
-
-    pip_desc.layout = {};
-    pip_desc.layout.buffers[0].stride = 28;
-    pip_desc.layout.attrs[ATTR_vs_position].format = SG_VERTEXFORMAT_FLOAT3;
-    pip_desc.layout.attrs[ATTR_vs_color0].format = SG_VERTEXFORMAT_FLOAT4;
-
-    pip_desc.depth = {};
-    pip_desc.depth.write_enabled = true;
-    pip_desc.depth.compare = SG_COMPAREFUNC_LESS_EQUAL;
-    sg_pipeline pip = sg_make_pipeline(&pip_desc);
-
-    sg_bindings bind_desc = {};
-    bind_desc.vertex_buffers[0] = vbuf;
-    bind_desc.index_buffer = ibuf;
-
-    state.gfx.pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
-    state.gfx.pip = pip;
-    state.gfx.bind = bind_desc;
-}
-
-void gui_init()
-{
-    simgui_desc_t simgui_desc = {};
-    simgui_desc.logger.func = slog_func;
-    simgui_setup(&simgui_desc);
-}
-
-void init()
-{
-    gfx_init();
-    gui_init();
-}
-
-void gui_draw()
-{
-    ImGui::SetNextWindowSize(ImVec2(0, 0));
-
-    ImGui::Begin("Hello, world!");
-    if (ImGui::RadioButton("Perspective", state.camera.projection == Projection::Perspective)) {
-        state.camera.projection = Projection::Perspective;
-    }
-    ImGui::SameLine();
-    if (ImGui::RadioButton("Orthographic", state.camera.projection == Projection::Orthographic)) {
-        state.camera.projection = Projection::Orthographic;
-    }
-    ImGui::SliderFloat("Rotation", &state.rotation_speed, 0.0f, 2.0f);
-    ImGui::ColorEdit3("Background", &state.gfx.clear_color.r);
-    if (ImGui::Button(sapp_is_fullscreen() ? "Switch to windowed" : "Switch to fullscreen")) {
-        sapp_toggle_fullscreen();
-    }
-    ImGui::End();
-}
-
-void frame()
-{
-    state.camera.update();
-
-    simgui_new_frame({
-        sapp_width(),
-        sapp_height(),
-        sapp_frame_duration(),
-        sapp_dpi_scale(),
-    });
-
-    vs_params_t vs_params;
-    const float t = (float)sapp_frame_duration();
-    state.rx += 1.0f * t * state.rotation_speed;
-    state.ry += 2.0f * t * state.rotation_speed;
-    glm::mat4 rxm = glm::rotate(glm::identity<glm::mat4>(), state.rx, glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 rym = glm::rotate(glm::identity<glm::mat4>(), state.ry, glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 model = rxm * rym;
-    vs_params.mvp = state.camera.proj_matrix() * state.camera.view_matrix() * model;
-    sg_range vs_range = SG_RANGE(vs_params);
-
-    state.gfx.pass_action.colors[0].clear_value = state.gfx.clear_color;
-
-    sg_pass pass = {};
-    pass.action = state.gfx.pass_action;
-    pass.swapchain = sglue_swapchain();
-
-    sg_begin_pass(&pass);
-    {
-        sg_apply_pipeline(state.gfx.pip);
-        sg_apply_bindings(&state.gfx.bind);
-        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, &vs_range);
-        sg_draw(0, 36, 1);
-
-        gui_draw();
-        simgui_render();
-    }
-    sg_end_pass();
-
-    sg_commit();
-}
-
-void cleanup()
-{
-    simgui_shutdown();
-    sg_shutdown();
-}
-
-void input(sapp_event const* event)
-{
-    if (event->type == SAPP_EVENTTYPE_KEY_DOWN) {
-        if (event->key_code == SAPP_KEYCODE_ESCAPE) {
-            sapp_quit();
-        }
-    }
-
-    if (simgui_handle_event(event)) {
-        return;
-    }
-
-    state.camera.handle_event(event);
-}
-
-sapp_desc sokol_main(int argc, char* argv[])
-{
-    (void)argc;
-    (void)argv;
-    sapp_desc desc = {};
-    desc.init_cb = init;
-    desc.frame_cb = frame;
-    desc.cleanup_cb = cleanup;
-    desc.event_cb = input;
-    desc.window_title = "Dear ImGui (sokol-app)";
-    desc.ios_keyboard_resizes_canvas = false;
-    desc.icon.sokol_default = true;
-    desc.enable_clipboard = true;
-    desc.logger.func = slog_func;
-    return desc;
+    Renderable cube(vertices, sizeof(vertices), indices, sizeof(indices), shader);
+    return cube;
 }
