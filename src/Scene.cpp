d33b159b0a7bb318be6f3c2a9044e8e635b8cf55
Adam Patterson <adam@adamrt.com>
2024-06-04 18:31:32 -0700

    Change Scene.renderables to be shared_ptrs

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,20 +1,20 @@
 #include "Scene.h"
 
-auto Scene::add_renderable(const Renderable& renderable) -> void
+auto Scene::add_renderable(std::shared_ptr<Renderable> renderable) -> void
 {
     renderables.push_back(renderable);
 }
 
 auto Scene::update(float delta_time, float rotation_speed) -> void
 {
-    for (auto& renderable : renderables) {
-        renderable.update(delta_time, rotation_speed);
+    for (auto renderable : renderables) {
+        renderable->update(delta_time, rotation_speed);
     }
 }
 
 auto Scene::render(const glm::mat4& view_proj) -> void
 {
     for (auto& renderable : renderables) {
-        renderable.render(view_proj);
+        renderable->render(view_proj);
     }
 }
--- src/Scene.h
+++ src/Scene.h
@@ -6,10 +6,10 @@
 
 class Scene {
 public:
-    auto add_renderable(const Renderable& renderable) -> void;
+    auto add_renderable(std::shared_ptr<Renderable> renderable) -> void;
     auto update(float delta_time, float rotation_speed) -> void;
     auto render(const glm::mat4& view_proj) -> void;
 
 private:
-    std::vector<Renderable> renderables;
+    std::vector<std::shared_ptr<Renderable>> renderables;
 };
