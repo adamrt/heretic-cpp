f0341d3a21a565d095f2904e5e5221f55c376c4f
Adam Patterson <adam@adamrt.com>
2024-06-15 11:37:54 -0700

    Add default color to ColoredModel

--- src/Model.h
+++ src/Model.h
@@ -54,7 +54,7 @@ public:
 
 class ColoredModel : public Model {
 public:
-    ColoredModel(std::shared_ptr<Mesh> _mesh, glm::vec4 _color, glm::vec3 _position = { 0.0f, 0.0f, 0.0f });
+    ColoredModel(std::shared_ptr<Mesh> _mesh, glm::vec4 _color = { 1.0f, 1.0f, 1.0f, 1.0f }, glm::vec3 _position = { 0.0f, 0.0f, 0.0f });
 
     auto render() -> void override;
 
